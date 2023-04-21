2cda03b46018478cc0d5215c399eb6978a5a4c70
JangHan Kim <kimjh741963@hanmail.net>
2023-04-21 10:45:15 +0900

    khj

--- SkillTree/Assets/Scripts/SkillUIManager.cs
+++ SkillTree/Assets/Scripts/SkillUIManager.cs
@@ -27,6 +27,7 @@ public class SkillUIManager : MonoBehaviour
         
         CreateDepthView();
         CreateRootSkill();
+        CreateRootLine();
         AllSkillDisable();
 
     }
@@ -57,7 +58,7 @@ public class SkillUIManager : MonoBehaviour
             SkillTree tree = dataLoader.skillTree[i];
             rootSkill.Add(Instantiate(skillPfab, depthList[tree.root.depth].transform));
             rootSkill[i].GetComponent<Skill>().SetSkill(tree);
-            rootSkill[i].name = "SKill[" + tree.root.code + "]";
+            rootSkill[i].name = tree.root.code.ToString();
 
             for (int j = 0; j < tree.root.children.Count; j++)
             {
@@ -76,15 +77,15 @@ public class SkillUIManager : MonoBehaviour
         {
             if(childSkill[i].GetComponent<Skill>().thisSkill.code == node.code)//이미생성된 스킬일경우
             {
-                pobj.GetComponent<SkillNodeLine>().target.Add(childSkill[i]);
+                //pobj.GetComponent<SkillNodeLine>().target.Add(childSkill[i]);
                 return;
             }
         }
         childSkill.Add(Instantiate(skillPfab, depthList[node.depth].transform));
         childSkill[childSkill.Count - 1].GetComponent<Skill>().thisSkill = node;
-        childSkill[childSkill.Count - 1].name = "SKill[" + node.code + "]";
+        childSkill[childSkill.Count - 1].name = node.code.ToString();
         Debug.Log("선생성 부모:" + pobj.name + "선생성 자식:" + childSkill[childSkill.Count - 1].name);
-        pobj.GetComponent<SkillNodeLine>().target.Add(childSkill[childSkill.Count - 1]);//선 노드생성
+        //pobj.GetComponent<SkillNodeLine>().target.Add(childSkill[childSkill.Count - 1]);//선 노드생성
         for (int i = 0; i < node.children.Count; i++)//자식의 자식노드 생성
         {
             SkillNode childNode = node.children[i];
@@ -92,16 +93,39 @@ public class SkillUIManager : MonoBehaviour
             {
                 if (childSkill[j].GetComponent<Skill>().thisSkill.code == childNode.code)//이미생성된 스킬일경우
                 {
-                    childSkill[j].GetComponent<SkillNodeLine>().target.Add(childSkill[j]);
+                    //childSkill[j].GetComponent<SkillNodeLine>().target.Add(childSkill[j]);
                     return;
                 }
             }
             childSkill.Add(Instantiate(skillPfab, depthList[childNode.depth].transform));
             childSkill[childSkill.Count - 1].GetComponent<Skill>().thisSkill = childNode;
-            childSkill[childSkill.Count - 1].name = "SKill[" + childNode.code + "]";
+            childSkill[childSkill.Count - 1].name = childNode.code.ToString();
             Debug.Log("선생성 부모:" + pobj.name + "선생성 자식:" + childSkill[childSkill.Count - 1].name);
-            childSkill[childSkill.Count - 1].GetComponent<SkillNodeLine>().target.Add(childSkill[childSkill.Count - 1]);
+            //childSkill[childSkill.Count - 1].GetComponent<SkillNodeLine>().target.Add(childSkill[childSkill.Count - 1]);
             PrintSkillTree(childSkill[childSkill.Count - 2], childNode);
+        }
+    }
+
+    void CreateRootLine()
+    {
+        for (int i = 0; i < rootSkill.Count; i++)
+        {
+            for (int j = 0; j < rootSkill[i].GetComponent<Skill>().thisSkill.children.Count; j++)
+            {
+                GameObject childObj = GameObject.Find(rootSkill[i].GetComponent<Skill>().thisSkill.children[j].code.ToString());
+
+                CreateChildLine(rootSkill[i], childObj);
+            }
+        }
+    }
+    void CreateChildLine(GameObject pobj, GameObject cobj)
+    {
+        pobj.GetComponent<SkillNodeLine>().target.Add(cobj);//선 노드생성
+        for (int i = 0; i < cobj.GetComponent<Skill>().thisSkill.children.Count; i++)//자식의 자식노드 생성
+        {
+            GameObject ccobj = GameObject.Find(cobj.GetComponent<Skill>().thisSkill.children[i].code.ToString());
+            cobj.GetComponent<SkillNodeLine>().target.Add(ccobj);
+            CreateChildLine(cobj, ccobj);
         }
     }
 
